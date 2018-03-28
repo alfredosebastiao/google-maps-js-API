37cee056d58a35ca348b38d4667fe93496ba261e
AlfredoSebastiao <stiaoalfredo@gmail.com>
2018-03-28 21:05:14 +0200

    hello map

--- Aula 1/_js/script.js
+++ Aula 1/_js/script.js
@@ -1,3 +1,21 @@
 /**
  * Created by User on 3/28/2018.
  */
+
+function initMap() {
+    var element = document.getElementById('mapa');
+
+    //options
+    var options = {
+        center : {
+            lat: -18.84291426212238,
+            lng: 35.80602505000002
+        },
+        zoom : 6
+
+    }
+
+    //
+    var map = new google.maps.Map(element,options);
+}
+
